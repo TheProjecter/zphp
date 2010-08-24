4b9f67b92cb5078d500339c70b38114e306bb46b
tv2knet <tv2knet@gmail.com>
2010-08-24 18:18:57 +0000

    * Added 2 smarty plugins : currenturl and url * Added a new way to output data : Response Writers (for example : return an instance of Z_Response_JSON to output the JSON code of the given data)

--- library/Z/Page.php
+++ library/Z/Page.php
@@ -2,17 +2,17 @@
 
 class Z_Page
 {
-	protected $_request;
-	protected $_response;
-	protected $_view;
+	public $request;
+	public $response;
+	public $view;
 	
 	public static $option_path = 'pages';
 	
 	public function __construct($request, $response, $view)
 	{
-		$this->_request = $request;
-		$this->_response = $response;
-		$this->_view = $view;
+		$this->request = $request;
+		$this->response = $response;
+		$this->view = $view;
 		
 		$this->preInit();	
 		$this->init();		
